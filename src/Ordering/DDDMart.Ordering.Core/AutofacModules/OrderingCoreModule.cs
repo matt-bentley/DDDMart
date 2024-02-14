28833ca89fb8b1586d1c53393ada24f065782708
Matt Bentley <mabentley@deloitte.co.uk>
2024-02-14 10:26:38 +0000

    upgrade to .net 8

--- src/Ordering/DDDMart.Ordering.Core/AutofacModules/OrderingCoreModule.cs
+++ src/Ordering/DDDMart.Ordering.Core/AutofacModules/OrderingCoreModule.cs
@@ -10,12 +10,6 @@ namespace DDDMart.Ordering.Core.AutofacModules
             // Register the DomainEventHandler classes (they implement INotificationHandler<>) in assembly holding the Domain Events
             builder.RegisterAssemblyTypes(ThisAssembly)
                 .AsClosedTypesOf(typeof(INotificationHandler<>));
-
-            builder.Register<ServiceFactory>(context =>
-            {
-                var componentContext = context.Resolve<IComponentContext>();
-                return t => { object o; return componentContext.TryResolve(t, out o) ? o : null; };
-            });
         }
     }
 }
